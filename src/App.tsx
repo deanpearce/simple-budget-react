3535d22a89d7d1c0e99a55a63947e897bcb6e341
Dean Pearce <deanpearce@gmail.com>
2018-01-04 21:47:48 -0500

    Experimenting with SemanticUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import './App.css';
+import { Button, Header } from 'semantic-ui-react';
 
 const logo = require('./logo.svg');
 
@@ -9,10 +10,19 @@ class App extends React.Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <Header size="huge" color="blue">
+            Welcome to Simple Budget
+          </Header>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <p>
+          <Button 
+            label="This is neat!"
+            content="Nifty Action"
+            icon="github"
+          />
         </p>
       </div>
     );
